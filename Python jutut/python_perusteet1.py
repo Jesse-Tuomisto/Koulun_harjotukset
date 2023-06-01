968a9e427af5639c5f13bfe037df4c6982f41232
LunaMoon335 <53067071+LunaMoon335@users.noreply.github.com>
2023-06-01 11:03:52 +0300

    Add files via upload

---
+++ Python jutut/python_perusteet1.py
@@ -0,0 +1,26 @@
+# 1.1
+# Kirjoita ohjelma, joka asettaa muuttujaan arvon ”Terve maailma” ja tulostaa sen muuttujan
+# 
+tincan="Terve maailma"
+print(tincan)
+#1.2
+#Kirjoita ohjelma, jossa asetetaan kahteen muuttujaan arvoiksi haluamasi luvut.
+#Tulosta niiden muuttujien summa.
+pullo=30
+huone=20
+print(pullo+huone)
+# 1.3
+# Kirjoita ohjelma, jossa lasketaan kolmen muuttujan keskiarvo. Tulosta lopputulos
+picatsu = 20
+flareon = 40
+gyarados = 10
+print ((picatsu + flareon + gyarados)/3)
+
+
+# 1.4
+# Kirjoita ohjelma, jossa muuttujaan asetetaan luku arvo, tulosta muuttuja. Sitten aseta muuttujan arvoksi,
+# jokin toinen luku ja tulosta se.
+vaunu = 70
+print (vaunu)
+vaunu = 50
+print (vaunu)
